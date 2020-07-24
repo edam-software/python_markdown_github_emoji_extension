1e318f52cf6604d5231d18a914e24944be07f1f8
ericdanielarellano <elericarellano@gmail.com>
2020-07-23 23:29:25 -0700

    use Path.open

--- ghe_emoji.py
+++ ghe_emoji.py
@@ -41,7 +41,7 @@ class GheEmoji(Extension):
         with requests.get(url, stream=True) as r:
             r.raise_for_status()
             try:
-                with open(Path(f"{SAVE_PATH}{tag}.png"), 'xb') as f:
+                with Path(f"{SAVE_PATH}{tag}.png").open('xb') as f:
                     for chunk in r.iter_content(chunk_size=8192):
                         # If you have chunk encoded response uncomment if
                         # and set chunk_size parameter to None.
@@ -49,7 +49,7 @@ class GheEmoji(Extension):
                         f.write(chunk)
             except FileExistsError as failed:
                 print(failed)
-                return
+                return`
 
     def download(self):
         for tag, url in self.getConfig('emoji').items():
