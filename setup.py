0b704e3e7725e55d60e88840069a296f7350e20f
Eric Arellano <ericarellano@github.com>
2022-09-18 13:27:24 -0500

    Update setup.py for Markdown 3.4.1

--- setup.py
+++ setup.py
@@ -3,22 +3,22 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
+
 setup(
-    name='ghe_emoji',
+    name='python_markdown_gh_emoji',
     version='0.9',
     packages=find_packages(),
-    py_modules=['ghe_emoji'],
-    install_requires=['markdown>=3.0', 'requests'],
-    python_requires='>3.7',
+    py_modules=['python_markdown_gh_emoji'],
+    install_requires=['markdown>=3.4', 'aiohttp'],
+    python_requires='>3.10',
     url='https://github.com/edam-software/github_emojis',
     license="OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
     author='EDAM',
     author_email='eric.arellano@hey.com',
-    description=""" Markdown extension to provide Githubs emojis in Pelican microblog 
-                    from the public API endpoint.
-                    No ownership of the emojis is implied nor included. """,
+    description='Markdown extension to provide Github emoji (in Pelican)',
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: GNU General Public License v3 or later (GPLv3+)",
         "Operating System :: OS Independent"]
 )
+
